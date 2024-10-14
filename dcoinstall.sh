ff9eafdd17d4730a9cbdb556258fb97eecdd7ac6
Bismarck Villca <bismarck.villca@gmail.com>
2024-10-13 22:58:49 -0400

    feat: improvenments during installation

--- dcoinstall.sh
+++ dcoinstall.sh
@@ -3,8 +3,17 @@ echo "|-----------------------------------|"
 echo "|--> Install docker-color-output <--|"
 echo "|-----------------------------------|"
 
-rm /usr/bin/docker-color-output
-wget -q https://github.com/devemio/docker-color-output/releases/download/2.3.0/docker-color-output-linux-amd64 -O /usr/bin/docker-color-output
+# Get the latest version URL from GitHub API
+LATEST_URL=$(curl -s https://api.github.com/repos/devemio/docker-color-output/releases/latest | grep "browser_download_url.*docker-color-output-linux-amd64" | cut -d '"' -f 4)
+
+# Remove old version
+rm -f /usr/bin/docker-color-output
+
+# Download the latest version
+wget -q $LATEST_URL -O /usr/bin/docker-color-output
+
+# Set permissions
 chmod 755 /usr/bin/docker-color-output
-echo "|--> docker-color-output installed successfull in /usr/bin"
+
+echo "|--> docker-color-output installed successfully in /usr/bin"
 docker-color-output
--- dockercolor_aliases.sh
+++ dockercolor_aliases.sh
@@ -5,7 +5,7 @@ dps() {
     docker ps "$@" | docker-color-output
 }
 dps1() {
-    docker ps --format "table {{.ID}}\\t{{.Names}}\\t{{.Status}}\\t{{.Image}}" "$@" | docker-color-output
+    docker ps --format "table {{.ID}}\\t{{.Names}}\\t{{.RunningFor}}\\t{{.Status}}\\t{{.Image}}" "$@" | docker-color-output
 }
 dpsports() {
     docker ps --format "table {{.ID}}\\t{{.Names}}\\t{{.Ports}}" "$@" | docker-color-output
@@ -17,13 +17,7 @@ dl() {
     docker logs -f "$@"
 }
 dlt() {
-    docker logs --tail 100 -f "$@"
-}
-dlt300() {
-    docker logs --tail 300 -f "$@"
-}
-dlt500() {
-    docker logs --tail 500 -f "$@"
+    docker logs --tail "${1:-100}" -f "$2"
 }
 
 # ==============================================================
@@ -39,11 +33,5 @@ dcl() {
     docker compose logs -f "$@"
 }
 dclt() {
-    docker compose logs --tail 100 -f "$@"
-}
-dclt300() {
-    docker compose logs --tail 300 -f "$@"
-}
-dclt500() {
-    docker compose logs --tail 500 -f "$@"
+    docker compose logs --tail "${1:-100}" -f "$2"
 }
