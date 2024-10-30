0599ba44f773ad277894a420c90d4bc82d3a845f
Bismarck Villca <bismarck.villca@gmail.com>
2024-10-30 08:38:21 -0400

    feat: added stats in settings

--- install.sh
+++ install.sh
@@ -10,15 +10,18 @@ DOCKER_COLOR_OUTPUT_FILE=$HOME/.docker_color_settings
 if [ -f "$DOCKER_COLOR_OUTPUT_FILE" ]; then
     rm $DOCKER_COLOR_OUTPUT_FILE
 fi
+echo -e "\033[1m➔ .Downloading .docker_color_settings in $DOCKER_COLOR_OUTPUT_FILE ⏳...\033[0m"
 wget -q https://raw.githubusercontent.com/villcabo/docker-color-output/main/dockercolor_aliases.sh -O $DOCKER_COLOR_OUTPUT_FILE
-echo "|--> .docker_color_settings successfull downloaded in $DOCKER_COLOR_OUTPUT_FILE"
+echo -e "\033[1m➔ .docker_color_settings successfull downloaded in $DOCKER_COLOR_OUTPUT_FILE ✔\033[0m"
 
 isInFile=$(cat $BASHRCFILE | grep -c '[[ -s "$HOME/.docker_color_settings" ]] && source "$HOME/.docker_color_settings"')
 if [ $isInFile -eq 0 ]; then
+    echo -e "\033[1m➔ Adding .docker_color_settings to $BASHRCFILE ⏳...\033[0m"
     echo '[[ -s "$HOME/.docker_color_settings" ]] && source "$HOME/.docker_color_settings"' >>$BASHRCFILE
-    echo "|--> .docker_color_settings successfull added to $BASHRCFILE"
+    echo -e "\033[1m➔ .docker_color_settings successfull added to $BASHRCFILE ✔\033[0m"
+
 else
-    echo "|--> .docker_color_settings already exists in $BASHRCFILE"
+    echo -e "\033[1m➔ .docker_color_settings already exists in $BASHRCFILE ✔\033[0m"
 fi
 #source $BASHRCFILE
 #source $DOCKER_COLOR_OUTPUT_FILE
